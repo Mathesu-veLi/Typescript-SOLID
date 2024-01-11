6db14701380948ecebc18badd19b94fac638c6b6
Matheus Levi <matheuslevit@gmail.com>
2024-01-11 18:32:28 -0300

    creates a "test" to the message class

--- src/dip/main.ts
+++ src/dip/main.ts
@@ -5,16 +5,26 @@ import { Product } from './classes/product';
 import { ShoppingCart } from './classes/shopping-cart';
 import { NoDiscount } from './classes/discount';
 import { EnterpriseCustomer } from './classes/customer';
+import { MessagingProtocol } from './classes/interfaces/messaging-protocol';
 
 //const fiftyPercentDiscount = new FiftyPercentDiscount();
 //const tenPercentDiscount = new TenPercentDiscount();
 const noDiscount = new NoDiscount();
 const shoppingCart = new ShoppingCart(noDiscount);
+
 const messaging = new Messaging();
+class MessagingMock implements MessagingProtocol {
+    sendMessage(): void {
+        console.log('A mensagem foi enviada pelo MOCK');
+    }
+}
+const messagingMock = new MessagingMock();
+
 const persistency = new Persistency();
+
 //const individualCustomer = new IndividualCustomer('Matheus', 'Silva', '111.111.111-11');
 const enterpriseCustomer = new EnterpriseCustomer('Enterprise', '22222222222222222');
-const order = new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
+const order = new Order(shoppingCart, messagingMock, persistency, enterpriseCustomer);
 
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 20.0));

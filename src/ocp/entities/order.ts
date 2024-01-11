d96d6aa2da8118018a231959cf7110eca6241ac0
Matheus Levi <matheuslevit@gmail.com>
2024-01-11 15:45:21 -0300

    add discount to total cart price

--- src/ocp/entities/order.ts
+++ src/ocp/entities/order.ts
@@ -24,7 +24,7 @@ export class Order {
 
         this._orderStatus = 'closed';
         this.messaging.sendMessage(
-            `Order with a total of R$${this.cart.total()} has been received`,
+            `Order with a total of R$${this.cart.totalWithDiscount()} has been received`,
         );
         this.persistency.saveOrder();
         this.cart.clear();
--- src/ocp/main.ts
+++ src/ocp/main.ts
@@ -12,7 +12,11 @@ const order = new Order(shoppingCart, messaging, persistency);
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 20.0));
 shoppingCart.addItem(new Product('Lápis', 0.9));
+
 console.log(shoppingCart.items);
+
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
 
 order.checkout();
 console.log(order.orderStatus);

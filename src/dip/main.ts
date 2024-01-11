2f4a47fd289bb58f6591f605f78a476a1838ab0f
Matheus Levi <matheuslevit@gmail.com>
2024-01-11 18:19:39 -0300

    copy files

---
+++ src/dip/main.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './classes/services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './classes/services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+//const individualCustomer = new IndividualCustomer('Matheus', 'Silva', '111.111.111-11');
+const enterpriseCustomer = new EnterpriseCustomer('Enterprise', '22222222222222222');
+const order = new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 20.0));
+shoppingCart.addItem(new Product('Lápis', 0.9));
+
+console.log(shoppingCart.items);
+
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+
+order.checkout();
+console.log(order.orderStatus);

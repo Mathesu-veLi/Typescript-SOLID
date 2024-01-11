beda1a6be0bfc184ccd9e10d941d56f4bcee9518
Matheus Levi <matheuslevit@gmail.com>
2024-01-11 18:13:00 -0300

    fix folder name

--- src/isp/main.ts
+++ src/isp/main.ts
@@ -4,6 +4,7 @@ import { Persistency } from './classes/services/persistency';
 import { Product } from './classes/product';
 import { ShoppingCart } from './classes/shopping-cart';
 import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
 
 //const fiftyPercentDiscount = new FiftyPercentDiscount();
 //const tenPercentDiscount = new TenPercentDiscount();
@@ -11,7 +12,9 @@ const noDiscount = new NoDiscount();
 const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+//const individualCustomer = new IndividualCustomer('Matheus', 'Silva', '111.111.111-11');
+const enterpriseCustomer = new EnterpriseCustomer('Enterprise', '22222222222222222');
+const order = new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
 
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 20.0));
